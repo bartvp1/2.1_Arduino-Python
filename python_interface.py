999de6f6a814431e93740a338694ffb9fb250eaa
bart <bartvp@live.nl>
2019-10-30 16:31:24 +0100

    update

--- python_interface.py
+++ python_interface.py
@@ -1,85 +1,104 @@
 from tkinter import *
+import tkinter.ttk as ttk
 from random import randint
 import time
 
 root = Tk()
 root.title('Interface')
 root.resizable(False, False)
+root.geometry("800x450")
 
-canvas = Canvas(root, width=1200, height=600, bg='grey')  # 0,0 is top left corner
-canvas.pack(expand=NO, fill=BOTH)
+main_color = '#3c3f41'
 
+left_canvas = Canvas(root, width=200, height=430, bg='#313335', highlightthickness=0)
+main_canvas = Canvas(root, width=600, height=430, bg=main_color, highlightthickness=0)
+status_bar = Canvas(root, width=800, height=20, bg='#2b2b2b', highlightthickness=0)
 
-canvas.create_line(175, 600, 175, 0, width=1)
+status_bar.place(x=0, y=430)
+left_canvas.place(x=0, y=0)
+main_canvas.place(x=200, y=0)
 
-button1 = Button(text='Controller 1', height = 3, width = 20)
-canvas.create_window(90, 100,  window=button1)
-button2 = Button(text='Controller 2', height = 3, width = 20)
-canvas.create_window(90, 180,  window=button2)
-button3 = Button(text='Controller 3', height = 3, width = 20)
-canvas.create_window(90, 260,  window=button3)
-button4 = Button(text='Controller 4', height = 3, width = 20)
-canvas.create_window(90, 340,  window=button4)
-button5 = Button(text='Controller 5', height = 3, width = 20)
-canvas.create_window(90, 420,  window=button5)
+configuration = [
+    {'manual': False, 'manual_extension': 50, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},  # controller 1
+    {'manual': False, 'manual_extension': 50, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},  # controller 2
+    {'manual': False, 'manual_extension': 50, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},  # controller 3
+    {'manual': False, 'manual_extension': 50, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},  # controller 4
+    {'manual': False, 'manual_extension': 50, 'min_extension': 0, 'max_extension': 100, 'threshold': 0}]  # controller 5
 
-'''
-canvas.create_rectangle(0, 0, 175, 120, outline="Black", fill="Grey")
-canvas.create_rectangle(0, 120, 175, 240, outline="Black", fill="Grey")
-canvas.create_rectangle(0, 240, 175, 360, outline="Black", fill="Grey")
-canvas.create_rectangle(0, 360, 175, 480, outline="Black", fill="Grey")
-canvas.create_rectangle(0, 480, 175, 600, outline="Black", fill="Grey")
-'''
+current_controller = -1
 
-label1 = Label(text='Automatische Bediening:', font=("Courier", 10), height = 3, width = 23, bg="grey")
-canvas.create_window(320, 75,  window=label1)
+def settings_panel(index):
+    global current_controller
+    main_canvas.delete("all")
+    draw_navigation()
+    current_controller = index
+    if current_controller != -1:
+        title = "Controller configuration "+str(current_controller+1)
+        label1 = Label(text=title, font='Courier 12 bold', bg=main_color, fg='#ffffff')
+        main_canvas.create_window(150, 50, window=label1)
 
+        min_label = Label(text='Min. uitrolstand (%):', font=("Courier", 8), fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
+        min_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
+        main_canvas.create_window(100, 100, window=min_label)
+        main_canvas.create_window(240, 90, window=min_slider)
 
-label2 = Label(text='Min. uitrolstant:', font=("Courier", 8), height = 3, width = 22, bg="grey")
-canvas.create_window(270, 140,  window=label2)
-slider = Scale(root, from_= 0, to=100, orient=HORIZONTAL, bg="grey", borderwidth ="0", highlightthickness = "0")
-canvas.create_window(400, 130, window=slider)
+        max_label = Label(text='Max. uitrolstand (%):', font=("Courier", 8), fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
+        max_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
+        main_canvas.create_window(100, 140, window=max_label)
+        main_canvas.create_window(240, 130, window=max_slider)
 
-label3 = Label(text='Max. uitrolstant:', font=("Courier", 8), height = 3, width = 22, bg="grey")
-canvas.create_window(270, 190,  window=label3)
-slider = Scale(root, from_= 0, to=100, orient=HORIZONTAL, bg="grey", borderwidth ="0", highlightthickness = "0")
-canvas.create_window(400, 180, window=slider)
+        drempel_label = Label(text='Drempelwaarde °C:', font='Courier 8', fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
+        drempel_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
+        main_canvas.create_window(100, 180, window=drempel_label)
+        main_canvas.create_window(240, 170, window=drempel_slider)
 
-label4 = Label(text='Drempelwaarde Temp:', font=("Courier", 8), height = 3, width = 22, bg="grey")
-canvas.create_window(270, 240,  window=label4)
-slider = Scale(root, from_= 0, to=100, orient=HORIZONTAL, bg="grey", borderwidth ="0", highlightthickness = "0")
-canvas.create_window(400, 230, window=slider)
+        manual_label = Label(text='Manual control', font='Courier 8', fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
+        manual_button = Checkbutton(root, selectcolor=main_color, bg=main_color, highlightthickness=0, activebackground=main_color, activeforeground='#ffffff', fg='#ffffff')
+        main_canvas.create_window(100, 240, window=manual_label)
+        main_canvas.create_window(200, 240, window=manual_button)
 
-button = Checkbutton(root, text="Handmatige bediening:", font=("Courier", 10), bg = "grey", borderwidth ="0", highlightthickness = "0", activebackground = "grey")
-canvas.create_window(320, 350,  window=button)
+        uitrol_label = Label(text='Uitrolstand:', font='Courier 8', height=3, width=22, fg='#ffffff', bg=main_color, anchor='w')
+        uitrol_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, fg='#ffffff', bg=main_color, borderwidth="0", highlightthickness=0)
+        main_canvas.create_window(100, 280, window=uitrol_label)
+        main_canvas.create_window(240, 270, window=uitrol_slider)
 
-label3 = Label(text='Uitrolstant:', font=("Courier", 8), height = 3, width = 22, bg="grey")
-canvas.create_window(270, 400,  window=label3)
-slider = Scale(root, from_= 0, to=100, orient=HORIZONTAL, bg="grey", borderwidth ="0", highlightthickness = "0")
-canvas.create_window(400, 390, window=slider)
+        apply_btn = Button(text='Apply', width=7, command=lambda: apply_settings())
+        main_canvas.create_window(260, 320, window=apply_btn)
 
-submitbutton = Button(text='Submit', height = 2, width = 15)
-canvas.create_window(400, 500,  window=submitbutton)
+def draw_graph():
+    main_canvas.create_line(600, 550, 1150, 550, width=2)  # x-axis
+    main_canvas.create_line(600, 550, 600, 50, width=2)  # y-axis
+    main_canvas.create_text(560, 520, angle="90", text="Value")
+    main_canvas.create_text(620, 580, text="Time")
 
+    # x-axis
+    for i in range(11):
+        x = 600 + (i * 50)
+        main_canvas.create_line(x, 550, x, 50, width=1, dash=(2, 5))
+        main_canvas.create_text(x, 550, text='%d' % (10 * i), anchor=N)
 
+    # y-axis
+    for i in range(11):
+        y = 550 - (i * 50)
+        main_canvas.create_line(600, y, 1150, y, width=1, dash=(2, 5))
+        main_canvas.create_text(590, y, text='%d' % (10 * i), anchor=E)
 
-canvas.create_line(600, 550, 1150, 550, width=2)  # x-axis
-canvas.create_line(600, 550, 600, 50, width=2)  # y-axis
-canvas.create_text(560, 520, angle="90", text="Value")
-canvas.create_text(620, 580, text="Time")
+def draw_navigation():
+    controller_buttons = [
+        Button(text='Controller 1', height=2, width=30, command=lambda: settings_panel(0)),
+        Button(text='Controller 2', height=2, width=30, command=lambda: settings_panel(1)),
+        Button(text='Controller 3', height=2, width=30, command=lambda: settings_panel(2)),
+        Button(text='Controller 4', height=2, width=30, command=lambda: settings_panel(3)),
+        Button(text='Controller 5', height=2, width=30, command=lambda: settings_panel(4))]
 
-# x-axis
-for i in range(11):
-    x = 600 + (i * 50)
-    canvas.create_line(x, 550, x, 50, width=1, dash=(2, 5))
-    canvas.create_text(x, 550, text='%d' % (10 * i), anchor=N)
+    offset = 60
+    for button in controller_buttons:
+        left_canvas.create_window(90, offset, window=button)
+        offset += 45
 
-# y-axis
-for i in range(11):
-    y = 550 - (i * 50)
-    canvas.create_line(600, y, 1150, y, width=1, dash=(2, 5))
-    canvas.create_text(590, y, text='%d' % (10 * i), anchor=E)
+def apply_settings():
+    #todo: send data to UNO
+    print('x')
 
-
-
+draw_navigation()
 root.mainloop()
