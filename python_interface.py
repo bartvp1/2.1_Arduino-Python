fd87fcbe27be3ec5cd7a75d228a9b94bfcf050fc
bart <bartvp@live.nl>
2019-10-30 20:49:59 +0100

    updated graph

--- python_interface.py
+++ python_interface.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import tkinter.ttk as ttk
 from random import randint
+from pprint import pprint
 import time
 
 root = Tk()
@@ -19,69 +20,81 @@ left_canvas.place(x=0, y=0)
 main_canvas.place(x=200, y=0)
 
 configuration = [
-    {'manual': False, 'manual_extension': 50, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},  # controller 1
-    {'manual': False, 'manual_extension': 50, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},  # controller 2
-    {'manual': False, 'manual_extension': 50, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},  # controller 3
-    {'manual': False, 'manual_extension': 50, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},  # controller 4
-    {'manual': False, 'manual_extension': 50, 'min_extension': 0, 'max_extension': 100, 'threshold': 0}]  # controller 5
+    {'manual': False, 'manual_extension': 0, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},  # controller 1
+    {'manual': False, 'manual_extension': 0, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},  # controller 2
+    {'manual': False, 'manual_extension': 0, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},  # controller 3
+    {'manual': False, 'manual_extension': 0, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},  # controller 4
+    {'manual': False, 'manual_extension': 0, 'min_extension': 0, 'max_extension': 100, 'threshold': 0}]  # controller 5
 
 current_controller = -1
+title = Label(text='Please a select controller', font='Courier 12 bold', bg=main_color, fg='#ffffff')
+main_canvas.create_window(150, 50, window=title)
 
 def settings_panel(index):
-    global current_controller
+    global current_controller  # , min_slider, max_slider, drempel_slider,uitrol_slider, manual_button, manual_var
     main_canvas.delete("all")
-    draw_navigation()
     current_controller = index
-    if current_controller != -1:
-        title = "Controller configuration "+str(current_controller+1)
-        label1 = Label(text=title, font='Courier 12 bold', bg=main_color, fg='#ffffff')
-        main_canvas.create_window(150, 50, window=label1)
+    title = Label(text="Controller "+str(current_controller+1)+" configuration", font='Courier 12 bold', bg=main_color, fg='#ffffff')
+    min_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
+    max_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
+    drempel_slider = Scale(root, from_=-30, to=50, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
+    uitrol_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, fg='#ffffff', bg=main_color, borderwidth="0", highlightthickness=0)
 
-        min_label = Label(text='Min. uitrolstand (%):', font=("Courier", 8), fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
-        min_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
-        main_canvas.create_window(100, 100, window=min_label)
-        main_canvas.create_window(240, 90, window=min_slider)
+    drempel_slider.set(configuration[current_controller]['threshold'])
+    max_slider.set(configuration[current_controller]['max_extension'])
+    min_slider.set(configuration[current_controller]['min_extension'])
+    uitrol_slider.set(configuration[current_controller]['manual_extension'])
+    manual_var = BooleanVar()
+    manual_button = Checkbutton(root, selectcolor=main_color, bg=main_color, highlightthickness=0, activebackground=main_color, activeforeground='#ffffff', fg='#ffffff', variable=manual_var)
 
-        max_label = Label(text='Max. uitrolstand (%):', font=("Courier", 8), fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
-        max_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
-        main_canvas.create_window(100, 140, window=max_label)
-        main_canvas.create_window(240, 130, window=max_slider)
+    if configuration[current_controller]['manual']:
+        manual_button.select()
+    else:
+        manual_button.deselect()
 
-        drempel_label = Label(text='Drempelwaarde °C:', font='Courier 8', fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
-        drempel_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
-        main_canvas.create_window(100, 180, window=drempel_label)
-        main_canvas.create_window(240, 170, window=drempel_slider)
+    min_label = Label(text='Min. uitrolstand (%):', font=("Courier", 8), fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
+    max_label = Label(text='Max. uitrolstand (%):', font=("Courier", 8), fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
+    drempel_label = Label(text='Drempelwaarde °C:', font='Courier 8', fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
+    manual_warning = Label(text="Manual control will override automatic behaviour", font='Courier 8', bg=main_color, fg='#ffffff')
+    manual_label = Label(text='Manual control', font='Courier 8', fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
+    uitrol_label = Label(text='Uitrolstand (%):', font='Courier 8', height=3, width=22, fg='#ffffff', bg=main_color, anchor='w')
+    apply_btn = Button(text='Apply', width=7, command=lambda: apply_settings([min_slider.get(), max_slider.get(), drempel_slider.get(), manual_var.get(), uitrol_slider.get()]))
 
-        manual_label = Label(text='Manual control', font='Courier 8', fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
-        manual_button = Checkbutton(root, selectcolor=main_color, bg=main_color, highlightthickness=0, activebackground=main_color, activeforeground='#ffffff', fg='#ffffff')
-        main_canvas.create_window(100, 240, window=manual_label)
-        main_canvas.create_window(200, 240, window=manual_button)
-
-        uitrol_label = Label(text='Uitrolstand:', font='Courier 8', height=3, width=22, fg='#ffffff', bg=main_color, anchor='w')
-        uitrol_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, fg='#ffffff', bg=main_color, borderwidth="0", highlightthickness=0)
-        main_canvas.create_window(100, 280, window=uitrol_label)
-        main_canvas.create_window(240, 270, window=uitrol_slider)
-
-        apply_btn = Button(text='Apply', width=7, command=lambda: apply_settings())
-        main_canvas.create_window(260, 320, window=apply_btn)
+    main_canvas.create_window(150, 50, window=title)
+    main_canvas.create_window(100, 100, window=min_label)
+    main_canvas.create_window(240, 90, window=min_slider)
+    main_canvas.create_window(100, 140, window=max_label)
+    main_canvas.create_window(240, 130, window=max_slider)
+    main_canvas.create_window(100, 180, window=drempel_label)
+    main_canvas.create_window(240, 170, window=drempel_slider)
+    main_canvas.create_window(190, 240, window=manual_warning)
+    main_canvas.create_window(100, 280, window=manual_label)
+    main_canvas.create_window(200, 280, window=manual_button)
+    main_canvas.create_window(100, 320, window=uitrol_label)
+    main_canvas.create_window(240, 310, window=uitrol_slider)
+    main_canvas.create_window(260, 360, window=apply_btn)
 
 def draw_graph():
-    main_canvas.create_line(600, 550, 1150, 550, width=2)  # x-axis
-    main_canvas.create_line(600, 550, 600, 50, width=2)  # y-axis
-    main_canvas.create_text(560, 520, angle="90", text="Value")
-    main_canvas.create_text(620, 580, text="Time")
+    posx = 400
+    posy = 120
+    width = 180
+    height = 80
+    main_canvas.create_line(posx, posy+height, posx+width, posy+height, width=1)        # x-axis
+    main_canvas.create_line(posx, posy+height, posx, posy, width=1)                     # y-axis
+    main_canvas.create_text(posx-15, posy-15, text="°C", font='Courier 8')              # y-unit
+    main_canvas.create_text(posx+30, posy+height+25, text="Time", font='Courier 8')     # x-unit
 
     # x-axis
-    for i in range(11):
-        x = 600 + (i * 50)
-        main_canvas.create_line(x, 550, x, 50, width=1, dash=(2, 5))
-        main_canvas.create_text(x, 550, text='%d' % (10 * i), anchor=N)
+    for i in range(0, 4):
+        x = 400 + (i * 60)
+        main_canvas.create_line(x, 200, x, 120, width=1, dash=(2, 5))
+        main_canvas.create_text(x, 210, text='%d' % (60 * i), font='Courier 6', anchor=N)
 
     # y-axis
-    for i in range(11):
-        y = 550 - (i * 50)
-        main_canvas.create_line(600, y, 1150, y, width=1, dash=(2, 5))
-        main_canvas.create_text(590, y, text='%d' % (10 * i), anchor=E)
+    for i in range(0, 5):
+        y = 200 - (i * 20)
+        main_canvas.create_line(400, y, 580, y, width=1, dash=(2, 5))
+        main_canvas.create_text(395, y, text='%d' % (20 * i), font='Courier 6', anchor=E)
 
 def draw_navigation():
     controller_buttons = [
@@ -96,9 +109,11 @@ def draw_navigation():
         left_canvas.create_window(90, offset, window=button)
         offset += 45
 
-def apply_settings():
+def apply_settings(obj):
     #todo: send data to UNO
-    print('x')
+    configuration[current_controller].update({'manual': obj[3], 'manual_extension': obj[4], 'min_extension': obj[0], 'max_extension': obj[1], 'threshold': obj[2]})
+    pprint(configuration[current_controller])
 
 draw_navigation()
+draw_graph()
 root.mainloop()
