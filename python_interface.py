9d6bafff13c57fad6d8b2e8e55ff881eecacabcd
bart <bartvp@live.nl>
2019-11-05 17:46:13 +0100

    controller id -> port id

--- python_interface.py
+++ python_interface.py
@@ -9,77 +9,78 @@ root.geometry("800x450")
 
 main_color = '#3c3f41'
 settings_panel_open = False
-current_controller = -1
+current_port = ""
 arduinos = {}
 
 left_canvas = Canvas(root, width=200, height=430, bg='#313335', highlightthickness=0)
 main_canvas = Canvas(root, width=600, height=430, bg=main_color, highlightthickness=0)
 status_bar = Canvas(root, width=800, height=20, bg='#2b2b2b', highlightthickness=0)
 
-configuration = [
-    {'manual': False, 'manual_extension': 0, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},
-    {'manual': False, 'manual_extension': 0, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},
-    {'manual': False, 'manual_extension': 0, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},
-    {'manual': False, 'manual_extension': 0, 'min_extension': 0, 'max_extension': 100, 'threshold': 0},
-    {'manual': False, 'manual_extension': 0, 'min_extension': 0, 'max_extension': 100, 'threshold': 0}]
+configuration = {}
 
 status_bar.place(x=0, y=430)
 left_canvas.place(x=0, y=0)
 main_canvas.place(x=200, y=0)
 
-def settings_panel(index):
-    global current_controller, settings_panel_open
-    settings_panel_open = not settings_panel_open
-    current_controller = index
-    sensor = arduinos.get(list(arduinos.keys())[current_controller])
-    title = Label(text=sensor+" configuration", font='Courier 12 bold', bg=main_color, fg='#ffffff', anchor=W)
-    min_slider = Scale(root, from_=0, to=100, resolution=5, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
-    max_slider = Scale(root, from_=min_slider.get(), to=100, resolution=5, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
-    drempel_slider = Scale()
-    uitrol_slider = Scale(root, from_=-30, to=50, resolution=5, orient=HORIZONTAL, fg='#ffffff', bg=main_color, borderwidth="0", highlightthickness=0)
 
-    drempel_slider.set(configuration[current_controller]['threshold'])
-    max_slider.set(configuration[current_controller]['max_extension'])
-    min_slider.set(configuration[current_controller]['min_extension'])
-    uitrol_slider.set(configuration[current_controller]['manual_extension'])
+def settings_panel(port):
+    global current_port, settings_panel_open
+    settings_panel_open = not settings_panel_open
+    current_port = port
+    sensor = arduinos.get(port)
+    title = Label(text=sensor+" configuration", font='Courier 12 bold', bg=main_color, fg='#ffffff', anchor=W)
+    sv = StringVar(root, 100)
+    maximale_uitrolstand_entry = Entry(root, textvariable=sv, width=4)
+    min_slider = Scale(root, from_=0, to=100, resolution=5, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
+    max_slider = Scale(root, from_=0, to=100, resolution=5, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
+    drempel_slider = Scale()
+    uitrol_slider = Scale(root, from_=0, to=100, resolution=5, orient=HORIZONTAL, fg='#ffffff', bg=main_color, borderwidth="0", highlightthickness=0)
     manual_var = BooleanVar()
     manual_button = Checkbutton(root, selectcolor=main_color, bg=main_color, highlightthickness=0, activebackground=main_color, activeforeground='#ffffff', fg='#ffffff', variable=manual_var)
 
-    if configuration[current_controller]['manual']:
+    drempel_slider.set(configuration[port]['threshold'])
+    max_slider.set(configuration[port]['auto_max_extension'])
+    min_slider.set(configuration[port]['auto_min_extension'])
+    uitrol_slider.set(configuration[port]['manual_extension'])
+
+    if configuration[port]['manual']:
         manual_button.select()
     else:
         manual_button.deselect()
 
-    min_label = Label(text='Min. uitrolstand (cm):', font=("Courier", 8), fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
-    max_label = Label(text='Max. uitrolstand (cm):', font=("Courier", 8), fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
+    maximale_uitrolstand_label = Label(text='Maximale hoogte (cm)', font=("Courier", 8), fg='#ffffff', height=3, width=22, bg=main_color, anchor=W)
+    min_label = Label(text='Min. uitrolstand (%)', font=("Courier", 8), fg='#ffffff', height=3, width=22, bg=main_color, anchor=W)
+    max_label = Label(text='Max. uitrolstand (%)', font=("Courier", 8), fg='#ffffff', height=3, width=22, bg=main_color, anchor=W)
     drempel_label = Label()
     manual_warning = Label(text="Manual control will override automatic behaviour", font='Courier 8', bg=main_color, fg='#ffffff')
-    manual_label = Label(text='Manual control', font='Courier 8', fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
-    uitrol_label = Label(text='Uitrolstand (cm):', font='Courier 8', height=3, width=22, fg='#ffffff', bg=main_color, anchor='w')
-    apply_btn = Button(text='Apply', width=7, command=lambda: apply_settings([min_slider.get(), max_slider.get(), drempel_slider.get(), manual_var.get(), uitrol_slider.get()]))
+    manual_label = Label(text='Manual control', font='Courier 8', fg='#ffffff', height=3, width=22, bg=main_color, anchor=W)
+    uitrol_label = Label(text='Uitrolstand (%)', font='Courier 8', height=3, width=22, fg='#ffffff', bg=main_color, anchor=W)
+    apply_btn = Button(text='Apply', width=7, command=lambda: apply_settings([sv.get(), min_slider.get(), max_slider.get(), drempel_slider.get(), manual_var.get(), uitrol_slider.get()]))
 
     if sensor == 'Lichtsensor':
-        drempel_label = Label(text='Drempelwaarde (lux):', font='Courier 8', fg='#ffffff', height=3, width=22, bg=main_color, anchor='e')
-        drempel_slider = Scale(root, from_=10000, to=100000, resolution=10000, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
+        drempel_label = Label(text='Drempelwaarde (lux)', font='Courier 8', fg='#ffffff', height=3, width=22, bg=main_color, anchor=W)
+        drempel_slider = Scale(root, from_=0, to=300, resolution=5, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
 
     if sensor == 'Temperatuursensor':
-        drempel_label = Label(text='Drempelwaarde (°C):', font='Courier 8', fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
+        drempel_label = Label(text='Drempelwaarde (°C)', font='Courier 8', fg='#ffffff', height=3, width=22, bg=main_color, anchor='w')
         drempel_slider = Scale(root, from_=-30, to=50, resolution=5, orient=HORIZONTAL, bg=main_color, fg='#ffffff', borderwidth="0", highlightthickness=0)
 
     main_canvas.delete("all")
-    main_canvas.create_window(170, 50, window=title)
-    main_canvas.create_window(100, 100, window=min_label)
-    main_canvas.create_window(240, 90, window=min_slider)
-    main_canvas.create_window(100, 140, window=max_label)
-    main_canvas.create_window(240, 130, window=max_slider)
-    main_canvas.create_window(100, 180, window=drempel_label)
-    main_canvas.create_window(240, 170, window=drempel_slider)
-    main_canvas.create_window(190, 240, window=manual_warning)
-    main_canvas.create_window(100, 280, window=manual_label)
-    main_canvas.create_window(200, 280, window=manual_button)
-    main_canvas.create_window(100, 320, window=uitrol_label)
-    main_canvas.create_window(240, 310, window=uitrol_slider)
-    main_canvas.create_window(260, 360, window=apply_btn)
+    main_canvas.create_window(145, 50, window=title)
+    main_canvas.create_window(100, 100, window=maximale_uitrolstand_label)
+    main_canvas.create_window(190, 100, window=maximale_uitrolstand_entry, anchor=W)
+    main_canvas.create_window(100, 140, window=min_label)
+    main_canvas.create_window(240, 130, window=min_slider)
+    main_canvas.create_window(100, 180, window=max_label)
+    main_canvas.create_window(240, 170, window=max_slider)
+    main_canvas.create_window(100, 220, window=drempel_label)
+    main_canvas.create_window(240, 210, window=drempel_slider)
+    main_canvas.create_window(190, 280, window=manual_warning)
+    main_canvas.create_window(100, 320, window=manual_label)
+    main_canvas.create_window(200, 320, window=manual_button)
+    main_canvas.create_window(100, 360, window=uitrol_label)
+    main_canvas.create_window(240, 350, window=uitrol_slider)
+    main_canvas.create_window(260, 400, window=apply_btn)
 
 def draw_graph():
     posx, posy, width, height = 400, 120, 180, 80
@@ -110,7 +111,12 @@ def refresh_arduinos():
         try:
             ser = serial.Serial(port, 9600)
             while ser.read():
-                line = str(ser.readline().lower().decode(encoding='UTF-8'))
+                print(ser.readline().decode())
+                try:
+                    line = str(ser.readline().lower().decode())
+                except:
+                    continue
+                print(line)
                 if arduinos.get(port) is None:
                     if line.find('licht') != -1:
                         arduinos.update({port: 'Lichtsensor'})
@@ -130,8 +136,8 @@ def draw_navigation():
         title = Label(text='Please connect a controller', font='Courier 12 bold', bg=main_color, fg='#ffffff')
         main_canvas.create_window(150, 50, window=title)
     else:
-        for i in range(len(arduinos.items())):
-            button = Button(text=arduinos.get(list(arduinos.keys())[i]), fg='white', bg='black', activebackground='black', activeforeground='white', height=2, width=30, command=lambda: settings_panel(i)),
+        for i in arduinos.keys():
+            button = Button(text=arduinos.get(i), fg='white', bg='black', activebackground='black', activeforeground='white', height=2, width=30, command=lambda: settings_panel(i)),
             left_canvas.create_window(90, offset, window=button)
             offset += 45
         if not settings_panel_open:
@@ -140,15 +146,17 @@ def draw_navigation():
             main_canvas.create_window(150, 50, window=title)
 
 def apply_settings(obj):
-    configuration[current_controller].update({'manual': obj[3], 'manual_extension': obj[4], 'min_extension': obj[0], 'max_extension': obj[1], 'threshold': obj[2]})
-    print(configuration[current_controller])
+    try:
+        uitrol = int(obj[0])
+    except:
+        uitrol = 100
+    configuration[current_port].update({'max_extension': uitrol, 'auto_min_extension': obj[1], 'auto_max_extension': obj[2], 'threshold': obj[3], 'manual': obj[4], 'manual_extension': obj[5]})
 
     # TODO: send data to UNO
-    current_port = list(arduinos.keys())[current_controller]
-    print(current_port)
+    print(current_port, configuration[current_port])
     ser = serial.Serial(current_port, 9600)
     ser.flushInput()
-    ser.write("a".encode())
+    ser.write("rollIn".encode())
 
 def draw_status():
     status_bar.delete("all")
